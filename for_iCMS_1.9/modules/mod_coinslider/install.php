a6e0c33714ec362206a370b9f93fee8836988f03
alexg <gavrilyuk82@gmail.com>
2020-02-22 22:04:49 +0800

    init

---
+++ for_iCMS_1.9/modules/mod_coinslider/install.php
@@ -0,0 +1,37 @@
+<?php
+function info_module_mod_coinslider(){
+        $_module['title']         = 'Cлайдер-галлерея Coin Slider';
+        $_module['name']          = 'Cлайдер-галлерея Coin Slider';
+        $_module['description']   = 'Cлайдер-галлерея Coin Slider';
+        $_module['link']          = 'mod_coinslider';
+        $_module['position']      = 'maintop';
+        $_module['author']        = 'soft-solution.ru';
+        $_module['version']       = '1';
+
+        $_module['config'] = array();
+        $_module['config']['shownum']='5';
+        $_module['config']['album_id']='0';
+        $_module['config']['effect']='random';
+        $_module['config']['width']='565';
+        $_module['config']['height']='290';
+        $_module['config']['showtitle']='1';
+        $_module['config']['shownav']='1';
+
+
+        return $_module;
+
+    }
+
+    function install_module_mod_coinslider(){
+
+        return true;
+
+    }
+
+    function upgrade_module_mod_coinslider(){
+
+        return true;
+
+    }
+
+?>
